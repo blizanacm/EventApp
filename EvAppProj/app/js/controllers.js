9a928e723fcbd2af78ad4f04ebdc063f71d8f16a
Milos Stankovic <milos.stankovic@fondmt.rs>
2014-06-23 23:30:10 +0200

    resource added.

--- EvAppProj/app/js/controllers.js
+++ EvAppProj/app/js/controllers.js
@@ -1,7 +1,7 @@
 var eventAppControllers = angular.module('EventAppControllers', []);
 
-eventAppControllers.controller('HomeCtrl', function($scope) {
-
+eventAppControllers.controller('HomeCtrl', function($scope, EventDataService) {
+    $scope.events = EventDataService.getAllEvents();
 });
 
 eventAppControllers.controller('CreateEventCtrl', function($scope) {
--- EvAppProj/app/js/services.js
+++ EvAppProj/app/js/services.js
@@ -1,3 +1,11 @@
-'use strict';
+var eventAppServices = angular.module('EventAppServices', []);
 
-angular.module('EventAppServices', []);
+eventAppServices.factory('EventDataService', function($resource){
+    var resource = $resource('/data/events/:id', {id: '@id'});
+    var getAll = function(){
+        return resource.query();
+    };
+    return {
+        getAllEvents: getAll
+    };
+});
