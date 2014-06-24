2163083e6c0a2ba1e2245f0605671c61c7730d82
Milos Stankovic <milos.stankovic@vicert.com>
2014-06-24 17:05:31 +0200

    sss

--- EvAppProj/app/js/controllers.js
+++ EvAppProj/app/js/controllers.js
@@ -1,6 +1,11 @@
 var eventAppControllers = angular.module('EventAppControllers', []);
 
 eventAppControllers.controller('HomeCtrl', function($scope, EventDataService) {
+    /*EventDataService.getAllEvents().then(function(data){
+        console.log(data);
+        $scope.events = data;
+    }, function(data){});*/
+
     $scope.events = EventDataService.getAllEvents();
 });
 
--- EvAppProj/app/js/services.js
+++ EvAppProj/app/js/services.js
@@ -1,8 +1,17 @@
 var eventAppServices = angular.module('EventAppServices', []);
 
-eventAppServices.factory('EventDataService', function($resource){
-    var resource = $resource('/data/events/:id', {id: '@id'});
+eventAppServices.factory('EventDataService', function($resource, $q){
+    var resource = $resource('/app/data/events/:id', {id: '@id'}, {'query': {method: 'GET', isArray: false}});
     var getAll = function(){
+        /*var deferred = $q.defer();
+        resource.get({id: '1.json'}, function(data){
+            deferred.resolve(data);
+        }, function(data){
+            deferred.reject(data);
+        });
+
+        return deferred.promise;*/
+
         return resource.query();
     };
     return {
